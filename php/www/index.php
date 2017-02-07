7fe58f62a5f3b8f27998b631a2b95b66d299bbc2
Musfiq Hossain <mhossain@localhost.localdomain>
2017-02-07 14:02:25 -0500

    if there is any result

--- php/www/index.php
+++ php/www/index.php
@@ -11,19 +11,22 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-} 
+}
 
 $sql = "SELECT * from pet";
 $result = $conn->query($sql);
 
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-       // echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
-	var_dump($row);
-    }
-} else {
-    echo "0 results";
+if($result) {
+  if ($result->num_rows > 0) {
+      // output data of each row
+      while($row = $result->fetch_assoc()) {
+         // echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+  	var_dump($row);
+      }
+  } else {
+      echo "0 results";
+  }
 }
+
 $conn->close();
 ?>
