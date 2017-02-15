8c13ded08b39a2b2ee3eecec0a393dd1b54e4632
mmi-rperez <rperez@managedmaint.com>
2017-02-15 11:52:43 -0500

    Fixed mongodb requirement issues

--- php/config/entrypoint.sh
+++ php/config/entrypoint.sh
@@ -1,6 +1,20 @@
-#!/bin/bash -x
+#!/bin/bash
 set -e
 
+# Setup permissions
+data_dir="/var/www/html"
+
+usermod -u 1000 www-data && groupmod -g 1000 www-data
+chown -R www-data:root "$data_dir"
+
+if  [ -d "$data_dir" ]; then
+    chgrp -R www-data "$data_dir"
+    chmod -R g+w "$data_dir"
+    find "$data_dir" -type d -exec chmod 2775 {} +
+    find "$data_dir" -type f -exec chmod ug+rw {} +
+fi
+
+# Enable rewrite
 a2enmod rewrite expires
 
 # Apache gets grumpy about PID files pre-existing
