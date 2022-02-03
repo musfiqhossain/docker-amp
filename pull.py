fed3c675431ea183e4fafb09c863c526fcffde2a
Musfiq Hossain <musfiq.hossain@cbsinteractive.com>
2022-02-03 11:30:27 -0500

    pull

--- pull.py
+++ pull.py
@@ -1,4 +1,22 @@
-import requests
-r = requests.get('https://api.github.com/repos/musfiqhossain/docker-amp/pulls/21/commits')
-t = r.json()
-print(t)
+import sys, subprocess, json, requests
+
+pr_commit_url = sys.argv[1]
+# 'https://api.github.com/repos/musfiqhossain/docker-amp/pulls/21/commits'
+response = requests.get(pr_commit_url)
+json_data = json.loads(response.text)
+
+
+print("arg ", sys.argv[1])
+
+for c in json_data:
+    sha =  c['sha']
+    if c['commit']['message'].find("Merge") == -1:
+            print("Checking commit ", c['commit']['message'])
+            proc = subprocess.Popen(['git', 'branch','--contains' , sha, '|', 'grep', 'qa'], stdout=subprocess.PIPE)
+            output = proc.stdout.read().decode('utf-8').strip()
+            if output == 'qa':
+                print('PR in QA')
+            else:
+                print('Cannot open PQ to main')
+    else:
+        print("skip merge", c['commit']['message'])
